f2eea98a4b27a6f1bad7c1e22d6698749bb2dbfc
MutableTuple <mutabletuple39@gmail.com>
2024-03-04 15:41:02 +0530

    made changes in navbar and added images in about, added button

--- herbs/src/components/NewArrivals/NewArrivals.component.jsx
+++ herbs/src/components/NewArrivals/NewArrivals.component.jsx
@@ -1,15 +1,55 @@
-import React from "react";
-import "../../../src/index.css";
-import NewSwiper from "./NewSwiper";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 import SwiperCard from "./SwiperCard.component";
-import "swiper/css";
+
 const NewArrivals = () => {
+  const swiperRef = useRef(null);
+
+  const goToPrevSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const goToNextSlide = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   return (
     <section className="container pt-5 mt-md-2 mt-lg-4 mt-xl-5">
+      <div className="position-relative z-2 d-flex justify-content-end pt-4 pb-4 mt-2 mt-sm-3 mt-md-4 mb-n5 mb-lg-4">
+        <button
+          className="btn btn-prev btn-icon btn-sm btn-outline-dark rounded-circle ms-3"
+          type="button"
+          id="new-prev"
+          aria-label="Previous slide"
+          tabIndex="0"
+          aria-controls="swiper-wrapper-122fae807f4b0341"
+          onClick={goToPrevSlide}
+        >
+          <i className="ai-arrow-left"></i>
+        </button>
+        <button
+          className="btn btn-next btn-icon btn-sm btn-outline-dark rounded-circle ms-3"
+          type="button"
+          id="new-next"
+          aria-label="Next slide"
+          tabIndex="0"
+          aria-controls="swiper-wrapper-122fae807f4b0341"
+          onClick={goToNextSlide}
+        >
+          <i className="ai-arrow-right"></i>
+        </button>
+      </div>
       <div className="row">
         <div className="col-lg-3 pb-3 mb-3 mb-md-4">
-          <h2 className="h1 mt-n2 mt-lg-0 mb-2 mb-lg-4">nieuwe aankomsten</h2>
+          <h2 className="h1 mt-n2 mt-lg-0 mb-2 mb-lg-4">New Arrivals</h2>
           <p className="fs-lg mb-4">
             Explore our new mouthwatering selection that we've recently
             introduced.
@@ -21,6 +61,7 @@ const NewArrivals = () => {
 
         <div className="col-lg-9">
           <Swiper
+            ref={swiperRef}
             spaceBetween={24}
             slidesPerView={1}
             loop={true}
@@ -31,6 +72,10 @@ const NewArrivals = () => {
               768: {
                 slidesPerView: 3,
               },
+            }}
+            navigation={{
+              prevEl: "#new-prev",
+              nextEl: "#new-next",
             }}
           >
             <SwiperSlide>
